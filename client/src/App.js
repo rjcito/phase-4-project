2aa30c006a60f75542688c9ec6eaf08bd407f076
Roberto Jacquez <rjqz92@gmail.com>
2023-01-06 21:17:07 -0700

    set up project

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Roberto's Phase 4 Project
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+          Project # 4
         </a>
       </header>
     </div>
