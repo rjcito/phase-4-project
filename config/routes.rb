b9ad17a30086eb125ada025a931a40a21cefdb80
Roberto Jacquez <rjqz92@gmail.com>
2023-01-09 17:35:26 -0700

    created my POST /login route inside routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :locations
   post "signup", to: "users#create"
   post "/me", to: "users#show"
+  post "/login", to: "sessions#create"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
